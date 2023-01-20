f1dcc582f131cf9cb2a014bcff5f21c7dfadee3e
Real-4339 <qvadym@stuba.sk>
2023-01-20 14:13:12 +0100

    update, epta

--- Client.py
+++ Client.py
@@ -1,98 +1,20 @@
 # Made by Vadym Tilihuzov
-"""
-Problem: if client has more than one network card, then it will be problem with finding ip and mask
-"""
-import socket
-import subprocess
-import os
-import nmap
-import ifcfg
+
+import socket, time
 
 
-multiple_networks = False
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+clientSocket.settimeout(1.0)
+message = b'test'
 
-def client_program():
-    global multiple_networks
+addr = ("127.0.0.1", 51200)
+start = time.time()
+clientSocket.sendto(message, addr)
 
-    # read from config file ip and port of server
-    port = "51200"  # socket server port number
-    port_int = int(port)
-
-    try:
-        # find ip and port of client
-        out = subprocess.Popen(('ifconfig'), stdout=subprocess.PIPE)
-        config = subprocess.check_output(('grep', 'broadcast'), stdin=out.stdout)
-        out.wait()
-        
-        # get values
-        ip = config.split()[1]
-        mask = config.split()[3]
-        # decoding ip and mask
-        ip = ip.decode("utf-8")
-        mask = mask.decode("utf-8")
-        # make lists
-        ip_list = ip.split(".")
-        mask_list = mask.split(".")
-        
-        # find network address
-        network = ""
-        for a in range(4):
-            if a == 3:
-                network += str(int(ip_list[a]) & int(mask_list[a]))
-            else:
-                network += str(int(ip_list[a]) & int(mask_list[a])) + "."    
-    except:
-        print("Error with finding ip and mask")
-        multiple_networks = True
-    
-    if multiple_networks == False:
-        # find all ip in network
-        devices = []
-        for device in os.popen('arp -a'): a = device.split()[1]; devices.append(a[1:len(a)-1])
-        # find ip of server
-        host = ""
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8", 80))
-        print(s.getsockname()[0])
-        s.close()
-        return
-        nm = nmap.PortScanner()
-        for a in devices:
-            nm.scan(a, port)
-            print(nm.all_hosts())
-            #print(nm[a]['tcp'][port_int]['state'])
-        #     if nm[a].state() == "up":
-        #         host = a
-        #         break
-        # print("Server ip:", host)
-        
-        return
-
-        client_socket = socket.socket()  # instantiate
-        client_socket.connect((host, port_int))  # connect to the server
-
-        message = input(" -> ")  # take input
-
-        while message.lower().strip() != 'bye':
-            client_socket.send(message.encode())  # send message
-            data = client_socket.recv(1486).decode()  # receive response
-
-            print('Received from server: ' + data)  # show in terminal
-
-            message = input(" -> ")  # again take input
-
-        client_socket.close()  # close the connection
-    else:
-        ip = input("Enter ip of server: ")
-        port = input("Enter port of server: ")
-
-        client_socket = socket.socket()  # instantiate
-        try:
-            client_socket.connect((ip, port))  # connect to the server
-        except:
-            print("Error with connecting to server, try using ip and port of server... NERD")
-            return
-        
-
-if __name__ == '__main__':
-    client_program()
+try:
+    data, server = clientSocket.recvfrom(1024)
+    end = time.time()
+    elapsed = end - start
+    print(f'{data} {elapsed}')
+except socket.timeout:
+    print('REQUEST TIMED OUT')
--- Server.py
+++ Server.py
@@ -1,49 +1,14 @@
 # Made by Vadym Tilihuzov
-"""
-Need to add thread that will be listening for new connections and connect them to the server
-Alse that first thread need to send configs between server and clients and read commands from cmd
 
----
-# dns_resolved_addr = socket.gethostbyname(host)
-# print("DNS resolved address:", dns_resolved_addr)
----
-
-"""
+import random
 import socket
-import sys
-import threading
-import time
 
-COUNT_OF_CLIENTS_TO_CONNECT = 2
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind(('', 51200))
 
-def server_listenig(): 
-    # get the hostname
-    host = socket.gethostname() # maybe need to use gethostbyname
-    port = "51200" # private port of server
-    port_int = int(port) 
-    
-    server_socket = socket.socket() # get instance
-    server_socket.bind((host, port_int))  # bind host address and port together
-
-    # configure how many client the server can listen
-    server_socket.listen(COUNT_OF_CLIENTS_TO_CONNECT)
-    conn, address = server_socket.accept()  # accept new connection
-
-    print("accept", str(conn), str(address))
-
-    while True:
-        # receive data stream. it won't accept data packet greater than 1486 bytes
-        data = conn.recv(1486).decode()
-        if not data: # remake
-            # if data is not received break
-            break
-        print("from connected user: " + str(data))
-        data = input(' -> ')
-        conn.send(data.encode())
-    
-    conn.close()  # close the connection
-    
-
-if __name__ == '__main__':
-    server_listenig()
-    
+while True:
+    rand = random.randint(0, 10)
+    message, address = server_socket.recvfrom(1024)
+    message = message.upper()
+    if rand >= 4:
+        server_socket.sendto(message, address)
