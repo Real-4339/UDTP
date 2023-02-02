f9ff4f92660d99092b625f7a9d0a7a004da48644
Real-4339 <qvadym@stuba.sk>
2023-02-02 21:12:24 +0100

    v8 created two threads listener and console

--- Host.py
+++ Host.py
@@ -1,11 +1,17 @@
 # Made by Vadym Tilihuzov
 # Date: 1.02.2023
 
-import random
+"""
+socket.recvfrom() returns a tuple (data, address)
+socker.recv() returns data
+"""
+
 import socket
+import threading
+import asyncio
 
 
-def connected (socket) -> bool:
+def connected (socket) -> bool: # TODO: recreate using my Packet class and Libuv
     try:
         socket.send(b'')
     except socket.error:
@@ -36,11 +42,42 @@ my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.bind(('', PORT)) # TODO: add ip handling
 my_socket.listen()
 
-# Function checking new connection
-def check_for_connection():
+# Function checking new connection # Useless
+def check_for_connection(): # TODO: recreate using my Packet class and Libuv
     client, address = my_socket.accept()
     clients.append(client)
     print(f'Connected with {str(address)}')
     client.send('You are now connected.'.encode('utf-8'))
 
+# Console loop, use yeild
+def console():
+    while True:
+        cmd = input('Enter command: ')
+        if cmd == 'exit':
+            my_socket.close()
+            exit()
+        elif cmd == 'list':
+            print(clients)
+        elif cmd == 'help':
+            print('Commands: list, exit')
+        else:
+            print('Unknown command')
+
+# Listener loop, use yeild
+def listener():
+    while True:
+        try:
+            data, addr = my_socket.recvfrom(1024) # TODO: 1024 is buffer size
+            print(data, data.decode())
+            # print(f'From {addr[0]}:{addr[1]}: {data.decode()}')
+        except:
+            pass
+
 # Main loop
+def V8():
+    print('Server started')
+    while True:
+        ...
+
+if __name__ == '__main__':
+    V8()
