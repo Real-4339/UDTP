eaa0cea4b4f8efa518022f1fe45df78a38937d5c
Real-4339 <qvadym@stuba.sk>
2023-09-03 23:30:13 +0200

    added seq num to packet

--- protocol/packet.py
+++ protocol/packet.py
@@ -13,9 +13,9 @@ LOGGER = logging.getLogger("Packet")
 
 
 class Packet:
-    ''' Todo: Add crc16 '''
-    def __init__(self, data: bytes, flags: Flags = None):
+    def __init__(self, data: bytes, flags: Flags = None, seq_num: int = None):
         self.__time_to_live: int = time.time()
+        self.__seq_num = seq_num
         self.__flags = flags
         self.__data = data
 
@@ -32,13 +32,13 @@ class Packet:
         return True
 
     @staticmethod
-    def construct(data: bytes, flags: Flags) -> bytes | None:
-        ''' Construct packet '''
+    def construct(data: bytes, flags: Flags, seq_num: int) -> bytes | None:
+        ''' Construct packet in Bytes '''
         crc16_func = crcmod.mkCrcFun(0x18005, rev=True, initCrc=0xFFFF, xorOut=0x0000)
         crc16 = crc16_func(data)
 
         try:
-            header = struct.pack('!BH', flags.value, crc16)
+            header = struct.pack('!BHB', flags.value, crc16, seq_num)
         except struct.error as e:
             LOGGER.error("Failed to pack header: %s", e)
             return None
@@ -47,30 +47,30 @@ class Packet:
 
     @staticmethod
     def deconstruct(data: bytes) -> 'Packet' or None:
-        ''' Deconstruct packet '''
-        if len(data) < 3:
+        ''' Deconstruct bytes to packet '''
+        if len(data) < 4:
             LOGGER.error("Packet is too short")
             return None
         
         try:
-            flags, crc16 = struct.unpack('!BH', data[:3])
+            flags, crc16, seq_num = struct.unpack('!BHB', data[:4])
         except struct.error as e:
             LOGGER.error("Failed to unpack header: %s", e)
             return None
 
-        packet_data = data[3:]
+        packet_data = data[4:]
         
-        return Packet(packet_data, Flags(flags))
+        return Packet(packet_data, Flags(flags), seq_num)
 
     @staticmethod
     def is_valid(data: bytes) -> bool:
         ''' Check if packet is valid '''
-        if len(data) < 3:
+        if len(data) < 4:
             LOGGER.error("Packet is too short")
             return False
         
-        flags, crc16 = struct.unpack('!BH', data[:3])    
-        packet_data = data[3:]
+        flags, crc16, seq_num = struct.unpack('!BHB', data[:4]) 
+        packet_data = data[4:]
 
         crc16_func = crcmod.mkCrcFun(0x18005, rev=True, initCrc=0xFFFF, xorOut=0x0000)
         computed_crc16 = crc16_func(packet_data)
