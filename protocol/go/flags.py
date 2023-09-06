4b5890fbe6b3d2bb07598b56c965e618020fdaa4
Real-4339 <qvadym@stuba.sk>
2023-09-06 19:52:21 +0200

    sack is now also keep alive

--- protocol/go/flags.py
+++ protocol/go/flags.py
@@ -4,9 +4,9 @@ from enum import IntFlag
 class Flags(IntFlag):
     SYN  = 0b00000001 # 1
     ACK  = 0b00000010 # 2
-    SACK = 0b00000100 # 4 Super ACK
+    SACK = 0b00000100 # 4 Super ACK | Keep Alive
     MSG  = 0b00001000 # 8 Message
-    INIT = 0b00010000 # 16 Init file transfer
+    FILE = 0b00010000 # 16 Init file transfer
     SR   = 0b00100000 # 32 (Send/Receive) Number of Send/Receive objects at the same time
     WM   = 0b01000000 # 64 Window Multiplier
     FIN  = 0b10000000 # 128
