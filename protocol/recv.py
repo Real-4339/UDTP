1a01cdeb0233816d31bc2ce06d941d276aff716b
Real-4339 <qvadym@stuba.sk>
2023-11-30 17:40:00 +0100

    fix: keep-alive

--- protocol/recv.py
+++ protocol/recv.py
@@ -11,20 +11,27 @@ from go.status import Status
 from go.adressinfo import AddressInfo
 
 
-''' Global variables '''
+""" Global variables """
 LOGGER = logging.getLogger("Receiver")
 
 
 class Receiver:
-    ''' 
-        Receiver class which will receive messages and files.
-        Devide data into packets if needed.
-        Handle sequence numbers.
-        Have buffer for packets.
-    '''
-    def __init__(self, send_func: Callable, addr: AddressInfo, 
-                 name: str = None, extention: str = None, 
-                 transfer_flag: int = None, fragment_size: int = 1468):
+    """
+    Receiver class which will receive messages and files.
+    Devide data into packets if needed.
+    Handle sequence numbers.
+    Have buffer for packets.
+    """
+
+    def __init__(
+        self,
+        send_func: Callable,
+        addr: AddressInfo,
+        name: str = None,
+        extention: str = None,
+        transfer_flag: int = None,
+        fragment_size: int = 1468,
+    ):
         self.__seq_num = 0
         self.__name = name
         self.__client = addr
@@ -32,10 +39,10 @@ class Receiver:
         self.__send_func = send_func
         self.__fragment_size = fragment_size
         self.__own_transfer_flag = transfer_flag
-        
+
         self.__acks: set[int] = set()
         self.__packets: list[Packet] = []
-        
+
         self.__alive = Status.ALIVE
         self.__last_time = time.time()
 
@@ -44,49 +51,49 @@ class Receiver:
         return self.__alive
 
     @property
-    def name (self) -> str:
+    def name(self) -> str:
         return self.__name
-    
+
     @property
     def ext(self) -> str:
         return self.__ext
-    
+
     @property
     def own_transfer_flag(self) -> Flags:
         return self.__own_transfer_flag
-    
+
     @property
     def fragment_size(self) -> int:
         return self.__fragment_size
-    
+
     @property
     def client(self) -> AddressInfo:
         return self.__client
-    
+
     def get_packets(self) -> list[Packet]:
-        ''' Get packets '''
-        
+        """Get packets"""
+
         return self.__packets
 
     def time_is_valid(self) -> bool:
-        ''' Check if receiver is still alive '''
-        
-        if time.time() - self.__last_time > Time.TTL:
+        """Check if receiver is still alive"""
+
+        if time.time() - self.__last_time > Time.KEEPALIVE:
             return False
-        
+
         return True
 
     def kill(self) -> None:
-        ''' Kill receiver '''
+        """Kill receiver"""
 
         self.__alive = Status.DEAD
 
     def receive(self, packet: Packet) -> None:
-        ''' Receive FILE, MSG, FIN '''
+        """Receive FILE, MSG, FIN"""
 
         if packet.flags == Flags.FILE:
             self._process_file(packet)
-            
+
         elif packet.flags == Flags.MSG:
             self._send_sack()
 
@@ -96,7 +103,7 @@ class Receiver:
         self.__seq_num += 1
 
     def receive_data(self, packet: Packet) -> None:
-        ''' Receive data '''
+        """Receive data"""
 
         if packet not in self.__packets:
             # LOGGER.info(f"Received data from {self.__client} : {packet.seq_num}")
@@ -108,21 +115,32 @@ class Receiver:
             self.__last_time = time.time()
 
         else:
-            ''' Resend ack '''
+            """Resend ack"""
             LOGGER.info(f"Resending ACK to {self.__client} : {packet.seq_num}")
-            ack = Packet.construct(data=f"{self.own_transfer_flag}".encode(), flags=Flags.ACK, seq_num=packet.seq_num)
+            ack = Packet.construct(
+                data=f"{self.own_transfer_flag}".encode(),
+                flags=Flags.ACK,
+                seq_num=packet.seq_num,
+            )
             self.__send_func(ack, self.__client)
 
     def _send_sack(self) -> None:
-        ''' Send SACK '''
+        """Send SACK"""
         LOGGER.info(f"Sending SACK to {self.__client}")
-        self.__send_func(Packet.construct(data=f"{self.own_transfer_flag}".encode(), flags=Flags.SACK, seq_num=self.__seq_num), self.__client)
+        self.__send_func(
+            Packet.construct(
+                data=f"{self.own_transfer_flag}".encode(),
+                flags=Flags.SACK,
+                seq_num=self.__seq_num,
+            ),
+            self.__client,
+        )
 
     def _process_file(self, packet: Packet) -> None:
-        ''' Process file '''
+        """Process file"""
 
         data = packet.data.decode().split(":")
-        
+
         name_ext, flag = data[0], int(data[1])
         name, ext = name_ext.split(".")
 
@@ -133,53 +151,60 @@ class Receiver:
         self._send_sack()
 
     def _process_fin(self, packet: Packet) -> None:
-        ''' Process FIN '''
+        """Process FIN"""
 
         self.__alive = Status.DEAD
 
         if self.name is not None and self.ext is not None:
-            ''' Create file from packets and save it '''
+            """Create file from packets and save it"""
             file_data = Packet.merge(self.__packets)
             file_name = f"{self.__name}_{int(time.time())}.{self.__ext}"
 
-            ''' Construct fpath '''
+            """ Construct fpath """
             script_dir = os.path.dirname(os.path.abspath(__file__))
-            full_file_path = os.path.join(script_dir, 'files', file_name)
+            full_file_path = os.path.join(script_dir, "files", file_name)
 
             try:
                 with open(full_file_path, "wb") as f:
                     f.write(file_data)
                 LOGGER.info(f"Received file from {self.__client}")
                 LOGGER.info(f"File name: {self.__name}_{int(time.time())}.{self.__ext}")
-                LOGGER.info(f"File size: {len(self.__packets) * self.fragment_size} bytes")
+                LOGGER.info(
+                    f"File size: {len(self.__packets) * self.fragment_size} bytes"
+                )
             except Exception as e:
                 LOGGER.error(f"Failed to save file: {e}")
 
         else:
-            ''' Create message from packets and print it '''
+            """Create message from packets and print it"""
             message = Packet.merge(self.__packets).decode()
             LOGGER.info(f"Received message from {self.__client} : {message}")
 
     def _acknowledge_data(self) -> None:
-        ''' Acknowledge data '''
-        
+        """Acknowledge data"""
+
         if len(self.__acks) == 0:
             # LOGGER.info(f"No data to acknowledge")
             return
-        
+
         for seq_num in self.__acks:
             # LOGGER.info(f"Sending ACK to {self.__client} : {seq_num}")
-            ack = Packet.construct(data=f"{self.own_transfer_flag}".encode(), flags=Flags.ACK, seq_num=seq_num)
+            ack = Packet.construct(
+                data=f"{self.own_transfer_flag}".encode(),
+                flags=Flags.ACK,
+                seq_num=seq_num,
+            )
             self.__send_func(ack, self.__client)
 
         self.__acks.clear()
-    
-    def _iterator(self) -> Status:
-        ''' Iterate over packets '''
 
-        if self.__alive == Status.DEAD:
+    def _iterator(self) -> Status:
+        """Iterate over packets"""
+
+        if self.__alive == Status.DEAD or not self.time_is_valid():
+            self.__alive = Status.DEAD
             return Status.FINISHED
-        
+
         self._acknowledge_data()
 
         return Status.SLEEPING
